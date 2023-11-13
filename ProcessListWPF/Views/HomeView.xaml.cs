bce61663c3f16d8f838365df0be36a9bb18ea349
Bartłomiej Skorupski <bartmann001@gmail.com>
2023-11-13 17:21:17 +0100

    change datagrid -> listview

--- ProcessListWPF/Views/HomeView.xaml.cs
+++ ProcessListWPF/Views/HomeView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
-
+using System.Windows.Data;
 
 namespace ProcessListWPF.Views;
 
@@ -10,6 +12,24 @@ public partial class HomeView : UserControl
     {
         InitializeComponent();
         MenuContentControl.Content = App.AppHost!.Services.GetRequiredService<MenuView>();
+    }
+
+    private void ListViewColumnHeaderClick(object sender, RoutedEventArgs e)
+    {
+        GridViewColumnHeader column = (GridViewColumnHeader)sender;
+        string sortBy = column.Tag.ToString()!;
+
+        ICollectionView view = CollectionViewSource.GetDefaultView(ProcessListView.ItemsSource);
+        ListSortDirection newDirection = ListSortDirection.Ascending;
+
+        if (view.SortDescriptions.Count > 0 && view.SortDescriptions[0].PropertyName == sortBy)
+        {
+            newDirection = (view.SortDescriptions[0].Direction == ListSortDirection.Ascending) ?
+                ListSortDirection.Descending : ListSortDirection.Ascending;
+        }
+
+        view.SortDescriptions.Clear();
+        view.SortDescriptions.Add(new SortDescription(sortBy, newDirection));
     }
 
 }
